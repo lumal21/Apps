402083715248067b9fd2d8f1c3dfb834e67b87c8
Alex Alexandre <alexalexandrejr@gmail.com>
2017-01-18 23:11:35 -0200

    Removing trash

--- uiot_web_application/javascript/message.js
+++
@@ -1,9 +0,0 @@
-function showErrorMessage(error){
-	alert(error.code);
-	alert(error.message);
-}
-
-function showSucessMessage(ok){
-	alert(ok.code);
-	alert(ok.message);
-}
--- uiot_web_application/javascript/real_time.js
+++
@@ -1,4 +0,0 @@
-/* 
-Here we will make some codes on real-time context.
-For it, a good js library is socket io, avalable on http://socket.io/   
-*/
