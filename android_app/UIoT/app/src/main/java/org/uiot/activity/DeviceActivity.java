a3c1efba7cb2a9c9cf1c502baa3980a1b03d9425
João Alencastro <joao.alencastro@uiot.org>
2017-02-22 19:40:31 -0300

    Fixed DeviceActivity bugs

--- android_app/UIoT/app/src/main/java/org/uiot/activity/DeviceActivity.java
+++ android_app/UIoT/app/src/main/java/org/uiot/activity/DeviceActivity.java
@@ -17,12 +17,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.json.JSONObject;
 import org.uiot.uiot.R;
-import org.uiot.utils.HttpUtils;
-
-import java.io.IOException;
-import java.util.HashMap;
 
 
 public class DeviceActivity extends Activity {
@@ -31,9 +26,7 @@ public class DeviceActivity extends Activity {
     private WifiManager wifiManager;
     private static String ipAddress = "";
     private static String macpAddress = "";
-    private HashMap<String, String> data;
-    private String url = "http://raise.uiot.org/devices";
-    private String method = "POST";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +38,8 @@ public class DeviceActivity extends Activity {
     }
 
     /**
-     * This's the most important method on this class, he call the ip/mac method,
-     * to verify the ip. And will create the intents to call a self-register device service
+     * This's the most important method on this class he call the ip/mac method,
+     * to verify the ip. And will create the intents to call a auto-register device service
      * and the sensors activity
      */
     private void buttonDevice(){
@@ -61,23 +54,6 @@ public class DeviceActivity extends Activity {
 
 //                Toast.makeText(getApplicationContext(), ipAddress, Toast.LENGTH_SHORT).show();
 //                Toast.makeText(getApplicationContext(), macpAddress, Toast.LENGTH_SHORT).show();
-
-
-
-                //Estou presisando saber como passar as request
-
-//                data = new HashMap<String, String>();
-//                data.put("name", deviceName.toString());
-//                data.put("chipset_id", ipAddress);
-//                data.put("mac", macpAddress);
-//
-//                try {
-//                    String retorno= HttpUtils.makeRequest(url,method,data);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-
-
 
                 startActivity(new Intent(DeviceActivity.this, SensorsActivity.class));
             }
@@ -116,11 +92,11 @@ public class DeviceActivity extends Activity {
     private void builderIpDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(R.string.ip_error_dialog_message)
-                .setTitle(R.string.msg_attention);
+                .setTitle(R.string.ip_error_dialog_title);
 
         final EditText input = new EditText(this);
         builder.setView(input);
-        builder.setPositiveButton(R.string.btn_continue,new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("Continuar",new DialogInterface.OnClickListener() {
 
             public void onClick(DialogInterface dialog, int which) {
 
@@ -130,9 +106,9 @@ public class DeviceActivity extends Activity {
 
         });
 
-        builder.setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener(){
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener(){
             public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), R.string.msg_cancel,
+                Toast.makeText(getApplicationContext(), "Canceloooou",
                         Toast.LENGTH_SHORT).show();
             }
         });
@@ -140,4 +116,14 @@ public class DeviceActivity extends Activity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+
+
+    /**
+     * Nexts steps
+     * 1 - create a button - ok
+     * 2 - create a intent to a service
+     * 3 - create a intenet to the list sensor activity
+     */
+
 }
