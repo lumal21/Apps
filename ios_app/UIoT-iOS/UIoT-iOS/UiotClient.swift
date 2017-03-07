dd5bfbcad4011e7322d761e9f8af9f97f975b00b
Camila <aliamcami@gmail.com>
2017-03-07 17:00:35 -0300

    Client and Service model classes implementation done

--- ios_app/UIoT-iOS/UIoT-iOS/UiotClient.swift
+++ ios_app/UIoT-iOS/UIoT-iOS/UiotClient.swift
@@ -12,77 +12,39 @@ import SwiftyJSON
 /**
  Example RAISe responses:
  
-{
-    "code": 200,
-    "clients": [
-    {
-    "name": "Raspberry PI",
-    "chipset": "AMD 790FX",
-    "mac": "FF:FF:FF:FF:FF:FF",
-    "serial": "C210",
-    "processor": "Intel I3",
-    "channel": "Ethernet",
-    "client_time": 1317427200,
-    "server_time": 1317987654
-    }
-    ]
-}
--------------------
-{
-    "code": 401,
-    "message": "Unauthorized"
-}
--------------------
-    {
-        "code": 403,
-        "message": "Forbidden"
-}
+ "name": "Raspberry PI",
+ "chipset": "AMD 790FX",
+ "mac": "FF:FF:FF:FF:FF:FF",
+ "serial": "C210",
+ "processor": "Intel I3",
+ "channel": "Ethernet",
+ "client_time": 1317427200,
+ "server_time": 1317987654
+ }
  */
 
- class UiotClient: NSObject {
-    var clientType : ClientType
-    var name : String = ""
-    var chipset : String = ""
-    var mac : String = ""
-    var serial : String = ""
-    var processor: String = ""
-    var channel : String = ""
-    var client_time : Double = 0
-    var server_time : Double = 0
+class UiotClient: NSObject {
+    var name : String
+    var chipset : String
+    var mac : String
+    var serial : String
+    var processor: String
+    var channel : String
+    var client_time : Double
+    var server_time : Double
     
     init(json : JSON) {
-        clientType = ClientType.init(rawValue: json["code"].intValue) ?? .NotIdentified
-        if clientType == .Authorized{
-            name = json["name"].stringValue
-            chipset = json["chipset"].stringValue
-            mac = json["mac"].stringValue
-            serial = json["serial"].stringValue
-            processor = json["processor"].stringValue
-            channel = json["channel"].stringValue
-            client_time = json["client_time"].doubleValue
-            server_time = json["server_time"].doubleValue
-        }
-    }
-    
-    enum ClientType : Int {
-        case Authorized = 200
-        case Unauthorized = 401
-        case Forbidden = 403
-        case NotIdentified = 0
+        name = json["name"].stringValue
+        chipset = json["chipset"].stringValue
+        mac = json["mac"].stringValue
+        serial = json["serial"].stringValue
+        processor = json["processor"].stringValue
+        channel = json["channel"].stringValue
+        client_time = json["client_time"].doubleValue
+        server_time = json["server_time"].doubleValue
         
-        var message : String{
-            switch self {
-            case .Authorized:
-                return "Authorized"
-            case .Forbidden:
-                return "Forbidden"
-            case .Unauthorized:
-                return "Unauthorized"
-            case .NotIdentified:
-                return "Client not identified"
-            }
-        }
     }
+
 }
 
 
---
+++ ios_app/UIoT-iOS/UIoT-iOS/UiotService.swift
@@ -0,0 +1,53 @@
+//
+//  UiotService.swift
+//  UIoT-iOS
+//
+//  Created by camila oliveira on 07/03/17.
+//  Copyright © 2017 UIoT. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+/**
+ RAISe json responses
+
+        {
+        "name": "Get temp",
+        "parameters": [
+            {"name": "Temp",
+            "type": "float"
+            },
+        }],
+        "return_type": "float",
+        "client_time": 1317427200,
+        "server_time": 1317987654
+        } */
+
+class UiotService: NSObject {
+
+    var name : String
+    var parameters : [Parameter]
+    var return_type : String
+    var client_time : Double
+    var server_time : Double
+    
+    init(json : JSON) {
+        name = json["name"].stringValue
+        return_type = json["return_type"].stringValue
+        client_time = json["client_time"].doubleValue
+        server_time = json["server_time"].doubleValue
+        parameters = []
+        for (_, js) in json["parameters"]{
+            parameters.append(Parameter(json: js))
+        }
+    }
+    
+    class Parameter{
+        var name : String
+        var type : String
+        init(json : JSON) {
+            name = json["name"].stringValue
+            type = json["type"].stringValue
+        }
+    }
+}
